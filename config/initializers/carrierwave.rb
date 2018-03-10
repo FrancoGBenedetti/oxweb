1c064ee2082bacc8b38ef220d0808b24a3aff5be
rgalazb <galaz.becerra@gmail.com>
2018-03-09 23:49:54 -0300

    s3 intento 1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,10 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'us-east-2'
   }
-  config.fog_directory = 'mh3bucket'
+  config.fog_directory = 'mh3folder'
   config.fog_public = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+  # config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
 end
