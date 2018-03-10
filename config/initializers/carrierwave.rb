9e10884ecec2125ddc5f5815403b2f1d81900470
rgalazb <galaz.becerra@gmail.com>
2018-03-09 22:58:41 -0300

    nombre del archivo carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
-
   config.fog_directory = 'mh3bucket'
   config.fog_public = false
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
