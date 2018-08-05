615449cc69f119bf5bcda3ba3fbbfd7eae2a401c
rgalazb <galaz.becerra@gmail.com>
2018-08-05 16:11:20 -0400

    mailer a contacto@oxchile.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default to: 'galaz.becerra@gmail.com'
+  default to: 'contacto@oxchile.com'
   def quotation(user)
     @user = user
     @orders = Order.where(user_id: user.id, payed: false)
