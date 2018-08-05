dc48f814d79ae69d3a23a7105b04aeb3435aadc3
rgalazb <galaz.becerra@gmail.com>
2018-08-05 00:46:28 -0400

    prueba mail desde

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: 'contacto@oxchile.com'
   default to: 'galaz.becerra@gmail.com'
   def quotation(user)
     @user = user
     @orders = Order.where(user_id: user.id, payed: false)
-    mail(subject: "cotización #{@user.email}")
+    mail(subject: "cotización #{@user.email}", from: @user.email)
   end
 end
