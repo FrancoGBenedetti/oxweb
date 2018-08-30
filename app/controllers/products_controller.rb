8005d38be278640b26b27987a03880f54c773bee
rgalazb <galaz.becerra@gmail.com>
2018-08-30 16:20:28 -0300

    sin validar usuario para descargar autocad

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy, :download_ficha]
-  before_action :authenticate_user!, except: [:show, :download_ficha]
+  before_action :authenticate_user!, except: [:show, :download_ficha, :download_autocad]
   layout 'admin', except: [:index, :show]
   # GET /products
   # GET /products.json
