068c478bfb6d5e1c008ce96b714317525d645294
rgalazb <galaz.becerra@gmail.com>
2018-08-30 17:01:37 -0300

    nombre archivo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -80,7 +80,7 @@ class ProductsController < ApplicationController
     cad = @product.autocad
     if cad.try(:file).exists?
       data = open(cad.url)
-      send_data data.read, type: data.content_type, x_sendfile: true, filename: @product.tipo + data.content_type
+      send_data data.read, type: data.content_type, x_sendfile: true, filename: "#{@product.tipo}.rar"
     end
   end
 
